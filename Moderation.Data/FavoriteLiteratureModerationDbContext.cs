2f93d9702fca8701a0588524022b7e04c0e69c74
Tsypin Ilya <tsypin.i.p@mail.ru>
2023-05-08 17:59:29 +0300

    feat: added GenericRepository and DbContext FQW-Moderation-4

--- Moderation.Data/FavoriteLiteratureModerationDbContext.cs
+++ Moderation.Data/FavoriteLiteratureModerationDbContext.cs
@@ -1,6 +1,27 @@
-﻿namespace FavoriteLiterature.Moderation.Data;
+﻿using FavoriteLiterature.Moderation.Data.Configurations;
+using FavoriteLiterature.Moderation.Data.Entities;
+using Microsoft.EntityFrameworkCore;
 
-public class FavoriteLiteratureModerationDbContext
+namespace FavoriteLiterature.Moderation.Data;
+
+public sealed class FavoriteLiteratureModerationDbContext : DbContext
 {
+    #region DbSets
+
+    public DbSet<Attachment> Attachments { get; set; } = null!;
     
+    public DbSet<Draft> Drafts { get; set; } = null!;
+
+    #endregion
+    
+    public FavoriteLiteratureModerationDbContext(DbContextOptions<FavoriteLiteratureModerationDbContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        new AttachmentConfiguration().Configure(modelBuilder.Entity<Attachment>());
+        new DraftConfiguration().Configure(modelBuilder.Entity<Draft>());
+    }
 }
--- Moderation.Data/Repositories/Common/IGenericRepository.cs
+++ Moderation.Data/Repositories/Common/IGenericRepository.cs
@@ -1,6 +1,14 @@
-﻿namespace FavoriteLiterature.Moderation.Data.Repositories.Common;
+﻿using System.Linq.Expressions;
+using FavoriteLiterature.Moderation.Data.Entities.Abstract;
 
-public interface IGenericRepository
+namespace FavoriteLiterature.Moderation.Data.Repositories.Common;
+
+public interface IGenericRepository<T> where T : BaseEntity
 {
-    
+    Task<T?> GetAsync(Expression<Func<T, bool>> expression, CancellationToken cancellationToken = default);
+    Task<IEnumerable<T>> GetAllAsync(int skip, int take, CancellationToken cancellationToken = default);
+    void Add(T entity);
+    void Update(T entity);
+    void Remove(T entity);
+    Task<bool> ExistsAsync(Expression<Func<T, bool>> expression, CancellationToken cancellationToken = default);
 }
