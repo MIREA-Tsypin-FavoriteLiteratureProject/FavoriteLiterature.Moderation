cdebcc73ccde37b6214326c446d0b2b769fe44fe
Tsypin Ilya <tsypin.i.p@mail.ru>
2023-05-08 18:08:43 +0300

    feat: added repositories for entities FQW-Moderation-4

---
+++ Moderation.Data/Repositories/Attachments/AttachmentsRepository.cs
@@ -0,0 +1,11 @@
+﻿using FavoriteLiterature.Moderation.Data.Entities;
+using FavoriteLiterature.Moderation.Data.Repositories.Common;
+
+namespace FavoriteLiterature.Moderation.Data.Repositories.Attachments;
+
+public class AttachmentsRepository : GenericRepository<Attachment>, IAttachmentsRepository
+{
+    public AttachmentsRepository(FavoriteLiteratureModerationDbContext dbContext) : base(dbContext)
+    {
+    }
+}
---
+++ Moderation.Data/Repositories/Attachments/IAttachmentsRepository.cs
@@ -0,0 +1,8 @@
+﻿using FavoriteLiterature.Moderation.Data.Entities;
+using FavoriteLiterature.Moderation.Data.Repositories.Common;
+
+namespace FavoriteLiterature.Moderation.Data.Repositories.Attachments;
+
+public interface IAttachmentsRepository : IGenericRepository<Attachment>
+{
+}
